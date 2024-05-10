09d5c5338d5a9d12a88632b35fdb971ad3b1cf4b
anikaweinmann <aweinmann@mundialis.de>
2024-05-10 10:07:03 +0200

    general_cleanup: try to fix region finding

--- src/grass_gis_helpers/cleanup.py
+++ src/grass_gis_helpers/cleanup.py
@@ -64,13 +64,15 @@ def general_cleanup(
         if os.path.isdir(rmdir):
             shutil.rmtree(rmdir)
     if orig_region is not None:
-        if grass.find_file(name=orig_region, element="windows")["file"]:
+        fing_reg = grass.find_file(name=orig_region, element="windows")
+        if "file" in fing_reg and fing_reg["file"]:
             grass.run_command("g.region", region=orig_region)
             grass.run_command(
                 "g.remove", type="region", name=orig_region, **kwargs
             )
     for rmreg in rm_regions:
-        if grass.find_file(name=rmreg, element="windows")["file"]:
+        fing_reg = grass.find_file(name=rmreg, element="windows")
+        if "file" in fing_reg and fing_reg["file"]:
             grass.run_command("g.remove", type="region", name=rmreg, **kwargs)
     strds = grass.parse_command("t.list", type="strds")
     mapset = grass.gisenv()["MAPSET"]
